4987019dd32e7bac113483618acba49bad46353e
Pratham dhobi <prathamdhobi45@gmail.com>
2023-09-20 19:38:01 +0530

    update HomeScreenActivity file

--- app/src/main/java/com/example/bmicalculator/HomeScreenActivity.java
+++ app/src/main/java/com/example/bmicalculator/HomeScreenActivity.java
@@ -1,24 +1,88 @@
 package com.example.bmicalculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 public class HomeScreenActivity extends AppCompatActivity {
-
-
+    NumberPicker feet_val,inch_val;
+    EditText age_val,weight_val;
+    Button BMI_val;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        NumberPicker feet_numpick,inch_numpick;
-        feet_numpick = findViewById(R.id.feet_numpick);
-        inch_numpick = findViewById(R.id.Inch_numpick);
-        feet_numpick.setMaxValue(10);
-        feet_numpick.setMinValue(2);
-        inch_numpick.setMaxValue(4);
-        inch_numpick.setMinValue(0);
+        feet_val = findViewById(R.id.feet_numpick);
+        inch_val = findViewById(R.id.Inch_numpick);
+        age_val = findViewById(R.id.age_edt);
+        weight_val = findViewById(R.id.weight_edt);
+        BMI_val = findViewById(R.id.BMI_btn);
+
+        // set max and min value for number picker
+        feet_val.setMaxValue(10);
+        feet_val.setMinValue(0);
+        inch_val.setMaxValue(11);
+        inch_val.setMinValue(0);
+
+        BMI_val.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Feet_Inch feet_inch=new Feet_Inch();
+                int age = Integer.parseInt(age_val.getText().toString());
+                int weight = Integer.parseInt(weight_val.getText().toString());
+                String msg;
+                int ft=feet_inch.feet_value;
+                int ich=feet_inch.inch_value;
+                float Total_feet = (float) (ft + (ich / 12.0f));
+                float meter = Total_feet * 0.304f;
+                float BMI = weight / (meter * meter);
+                if(BMI<19.0f){
+                    msg="Under Weight";
+                }else if(BMI>=19.0f && BMI<=25.0f){
+                    msg="Normal";
+                }else{
+                    msg="Over Weight";
+                }
+                AlertDialog.Builder builder=new AlertDialog.Builder(HomeScreenActivity.this);
+                builder.setTitle("Result");
+                builder.setMessage("Your BMI number is "+ BMI +"\n"+msg);
+                builder.setCancelable(true);
+                builder.setPositiveButton("OK", (DialogInterface.OnClickListener) (dialog, which)->{
+                    finish();
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
+class Feet_Inch{
+    int feet_value;
+    int inch_value;
+    public void feet(){
+        HomeScreenActivity homeScreenActivity = new HomeScreenActivity();
+        homeScreenActivity.feet_val.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int old_val, int curr_val) {
+                 feet_value = curr_val;
+            }
+        });
+    }
+    public void inch(){
+        HomeScreenActivity homeScreenActivity=new HomeScreenActivity();
+        homeScreenActivity.inch_val.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int old_val, int curr_val) {
+                inch_value = curr_val;
+            }
+        });
     }
 }
